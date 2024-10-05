57df63d1fa82473143f2bb4768d1564ccb6ddb77
CraftGamer9651 <140124034+CraftGamer9651@users.noreply.github.com>
2024-10-05 15:57:37 -0700

    Add files via upload

--- student.py
+++ student.py
@@ -1,27 +1,48 @@
-students = ['Howard', 'callem', 'christian']
-for i in range(len(students)):
-    students[i] = students[i].upper()
+class Student:
+    def __init__(self,name,student_id,grade_level,math,science,history,english):
+        self.name = name
+        self.student_id = student_id
+        self.grade_level = grade_level
+        self.math = math
+        self.science = science
+        self.history = history
+        self.english = english
+        self.grades = [math,science,history,english]
 
-def find_student(student):
-    for s in students:
-        if s == student.upper():
-            return True
-    return False
+    def update_grade(self,subject_I,grade):
+        self.grades[subject_I] = grade
+    
+    def get_gpa(self,):
+        grade_sum = 0.0
+        for g in self.grades:
+            if g == "A":
+                grade_sum += 4
+            elif g == "B":
+                grade_sum += 3
+            elif g == "C":
+                grade_sum += 2
+            elif g == "D":
+                grade_sum += 1
+        return grade_sum / len(self.grades)
+    
+    def student_info(self):
+        print("Name: " + self.name)
+        print("Id: " + str(self.student_id))
+        print("Grades: " + ', '.join(self.grades))
+        print("GPA: " + str(self.get_gpa()))
 
-def remove_student(student):
-    for s in students:
-        if s == student.upper():
-            students.remove(s)
+    def to_list(self):
+        return [self.name, self.student_id, self.grade_level, self.math, self.science, self.history, self.english]
 
+    @staticmethod
+    def from_list(data):
+        name, student_id, grade_level, math, science, history, english = data
+        return Student(name, student_id, grade_level, math, science, history, english)
 
-def add_student(student):
-    students.append(student.upper())
-
-new_students = {}
-new_students['name'] = 10
-print(new_students)
-
-add_student("john cena")
-
-for s in students:
-    print(s)
+if __name__ == "__main__":
+    callem = Student("Callem", 12703)
+    callem.update_grade(0,"A")
+    callem.update_grade(1,"B")
+    callem.update_grade(2,"B")
+    callem.update_grade(3,"A")
+    callem.student_info()
